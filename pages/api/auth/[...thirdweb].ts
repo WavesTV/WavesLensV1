cbc30ea03b969144518291c5e507e32b0c6f6df6
GageVanK <87737875+GageVanK@users.noreply.github.com>
2023-10-07 00:34:24 +0000

    t

--- pages/api/auth/[...thirdweb].ts
+++ pages/api/auth/[...thirdweb].ts
@@ -4,7 +4,7 @@ import { ethers } from "ethers";
 
 export const { ThirdwebAuthHandler, getUser } = ThirdwebAuth({
   // Use the domain from the environment or default to evmkit.com
-  domain: "waves-lensv1.vercel.app",
+  domain: "https://waves-lensv1.vercel.app",
 
   // Use the private key from the environment or generate a random one
   wallet: "uI4kVapLEYRj_LF7RTreiPdi5hcb3doHLTFrsJRPPkOsv9ETkgzDZKpeUA32_SXMeE4eq5fJxrS8IcYBfA9PBA"
