8d9582f9a6844c4f0a9385346ad679fd6944a332
WavesTV <148727170+WavesTV@users.noreply.github.com>
2023-10-27 00:34:40 +0000

    e

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -46,7 +46,7 @@ function LensThirdwebProvider({ children }: { children: React.ReactNode }) {
   const signer = useSigner();
   const router = useRouter();
   const signerWrapped = useTypedDataSignerWrapper(signer, sdk);
-if (!signer && router.pathname !== "/" && router.pathname !== "/why" && router.pathname !== "/profile") {
+if (!signer && !/^\/(why|profile(\/|$))/.test(router.pathname)) {
     return (
       <>
         <NetworkSwitchModal />
