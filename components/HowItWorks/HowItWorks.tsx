e21d7bd345a517bd59d13039da430f4e8ef3c09a
WavesTV <148727170+WavesTV@users.noreply.github.com>
2023-10-27 00:07:17 +0000

    yethirr

---
+++ components/HowItWorks/HowItWorks.tsx
@@ -0,0 +1,30 @@
+import { Container, Text } from '@mantine/core';
+import classes from './HowItWorks.module.css';
+import { Zoom } from "react-awesome-reveal";
+
+const data = [
+  {
+    title: 'Go Live',
+    description: 'Once you have a Lens Handle you can Livestream and Post onchain, making your content available across all Lens Apps.',
+  },
+  {
+    title: 'Earn',
+    description: 'Earn via Subcriptions, Tips, and NFT Sales.',
+  },
+  {
+    title: 'Build',
+    description: 'Waves is a place for Streamers to maximize their visibility and earnings. Start growing you Wave today!',
+  },
+];
+
+export function HowItWorks() {
+  const stats = data.map((stat) => (
+    <div key={stat.title} className={classes.stat}>
+        <Zoom>
+      <Text className={classes.count}>{stat.title}</Text>
+      <Text className={classes.description}>{stat.description}</Text>
+      </Zoom>
+    </div>
+  ));
+  return <Container className={classes.root}>{stats}</Container>;
+}
